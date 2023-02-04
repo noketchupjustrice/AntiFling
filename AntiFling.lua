a0bdd23d0c53628716d844bf761baf2e604ef92b
Lucid1337 <101951687+noketchupjustrice@users.noreply.github.com>
2023-02-03 19:07:52 -0800

    Update AntiFling.lua

--- AntiFling.lua
+++ AntiFling.lua
@@ -1,167 +1,89 @@
--- Made by noketchupjustrice#3666
--- Added more waits and made the code look more stupid
--- Will update and make the code look cleaner when i have time
-local Players = game:GetService("Players")
+--noketchupjustrice#3666
+--Everything should be fixed was just debating wether I should release this or not.
+if not game:IsLoaded() then
+	game.IsLoaded:Wait()
+end;
+
+local Players = game:service("Players")
 local LocalPlayer = Players.LocalPlayer
 
-local Debris = game:GetService("Debris") -- :scream:
+if not LocalPlayer.Character then
+	LocalPlayer.CharacterAdded:Wait()
+end;
 
-for i,v in pairs(game.workspace:GetDescendants()) do
-    if v:IsA("NoCollisionConstraint") then
-        Debris:AddItem(v, 1/0)
-    end
-end
+local CheckInstance = function(Part)
+	if Part.ClassName:lower():find("part") then
+        local BaseInstance = Part;
+		pcall(function()
+			for i, Part in pairs(LocalPlayer.Character:GetDescendants()) do
+				if Part.ClassName:lower():find("part") then
+					local NoCollisionConstraint = Instance.new("NoCollisionConstraint", BaseInstance)
+                    NoCollisionConstraint.Enabled = true
+					NoCollisionConstraint.Part0 = BaseInstance;
+					NoCollisionConstraint.Part1 = Part;
+				end
+			end
+		end)
+	end
+end;
 
-function NoCol()
-    for i,Player in pairs(Players:GetPlayers()) do
-        if Player ~= LocalPlayer then
-            if not Player then
-                repeat task.wait() until Player
-            end
-            if not Player.Character then
-                repeat task.wait() until Player.Character
-            end
-            if not Player.Character:FindFirstChildOfClass("Humanoid") then
-                repeat task.wait() until Player.Character:FindFirstChildOfClass("Humanoid")
-            end
-            if not Player.Character:FindFirstChild("HumanoidRootPart") then
-                repeat task.wait() until Player.Character:FindFirstChild("HumanoidRootPart")
-            end
-            if not Player.Character:FindFirstChild("Head") then
-                repeat task.wait() until Player.Character:FindFirstChild("Head")
-            end
-            if not LocalPlayer then
-                repeat task.wait() until LocalPlayer
-            end
-            if not LocalPlayer.Character then
-                repeat task.wait() until LocalPlayer.Character
-            end
-            if not LocalPlayer.Character:FindFirstChildOfClass("Humanoid") then
-                repeat task.wait() until LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-            end
-            if not LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-                repeat task.wait() until LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-            end
-            if not LocalPlayer.Character:FindFirstChild("Head") then
-                repeat task.wait() until LocalPlayer.Character:FindFirstChild("Head")
-            end
-            for i,TargetPart in pairs(Player.Character:GetDescendants()) do
-                if TargetPart:IsA("BasePart") then
-                    for i,v in pairs(LocalPlayer.Character:GetDescendants()) do
-                        if v:IsA("BasePart") then
-                            if not v:FindFirstChild("NoCollisionConstraint") then
-                                local NoCol = Instance.new("NoCollisionConstraint", TargetPart)
-                                NoCol.Enabled = true; NoCol.Part0 = v; NoCol.Part1 = TargetPart;
-                                coroutine.wrap(function()
-                                    Player.CharacterAdded:Connect(function(PlayerCharacter)
-                                        if not Player then
-                                            repeat task.wait() until Player
-                                        end
-                                        if not PlayerCharacter then
-                                            repeat task.wait() until PlayerCharacter
-                                        end
-                                        if not PlayerCharacter:FindFirstChildOfClass("Humanoid") then
-                                            repeat task.wait() until PlayerCharacter:FindFirstChildOfClass("Humanoid")
-                                        end
-                                        if not PlayerCharacter:FindFirstChild("HumanoidRootPart") then
-                                            repeat task.wait() until PlayerCharacter:FindFirstChild("HumanoidRootPart")
-                                        end
-                                        if not PlayerCharacter:FindFirstChild("Head") then
-                                            repeat task.wait() until PlayerCharacter:FindFirstChild("Head")
-                                        end
-                                        if not LocalPlayer then
-                                            repeat task.wait() until LocalPlayer
-                                        end
-                                        if not LocalPlayer.Character then
-                                            repeat task.wait() until LocalPlayer.Character
-                                        end
-                                        if not LocalPlayer.Character:FindFirstChildOfClass("Humanoid") then
-                                            repeat task.wait() until LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-                                        end
-                                        if not LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-                                            repeat task.wait() until LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-                                        end
-                                        if not LocalPlayer.Character:FindFirstChild("Head") then
-                                            repeat task.wait() until LocalPlayer.Character:FindFirstChild("Head")
-                                        end
-                                        for i,v in pairs(game.Players:GetPlayers()) do
-                                            if v ~= LocalPlayer then
-                                                for i,DestroyPart in pairs(v.Character:GetDescendants()) do
-                                                    if v:IsA("NoCollisionConstraint") then
-                                                        Debris:AddItem(v, 1/0)
-                                                    end
-                                                end
-                                            end
-                                        end
-                                        NoCol()
-                                    end)()
-                                end)
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
-end
-
-NoCol()
-
-game.Players.PlayerAdded:Connect(function(Player)
-    if not Player then
-        repeat task.wait() until Player
-    end
-    if not Player.Character then
-        repeat task.wait() until Player.Character
-    end
-    if not Player.Character:FindFirstChildOfClass("Humanoid") then
-        repeat task.wait() until Player.Character:FindFirstChildOfClass("Humanoid")
-    end
-    if not Player.Character:FindFirstChild("HumanoidRootPart") then
-        repeat task.wait() until Player.Character:FindFirstChild("HumanoidRootPart")
-    end
-    if not Player.Character:FindFirstChild("Head") then
-        repeat task.wait() until Player.Character:FindFirstChild("Head")
-    end
-    for i,v in pairs(game.Players:GetPlayers()) do
-        if v ~= LocalPlayer then
-            if not v.Character then
-                repeat task.wait() until v.Character
-            end
-            for i,DestroyPart in pairs(v.Character:GetDescendants()) do
+local Insert = function(Target, Bool)
+	local Start = function()
+		local Inserted = {}
+		if not Target.Character then
+			Target.CharacterAdded:Wait()
+		end;
+		Target.Character:WaitForChild("Head", 5)
+		Target.Character:WaitForChild("Humanoid", 5)
+        pcall(function()
+            for i, v in pairs(Target.Character:GetDescendants()) do
                 if v:IsA("NoCollisionConstraint") then
-                    Debris:AddItem(v, 1/0)
+                    v:Destroy()
                 end
-            end
-        end
-    end
-    NoCol()
-end)
+            end;
+            for i, v in pairs(Target.Character:GetDescendants()) do
+                CheckInstance(v)
+            end;
+            Target.Character.DescendantAdded:Connect(function(v)
+                if not table.find(Inserted, v) then
+                    table.insert(Inserted, v)
+                    CheckInstance(v)
+                end
+            end)
+        end)
+	end;
+	pcall(Start)
+	if not Bool then
+		Target.CharacterAdded:Connect(Start)
+	end
+end;
 
-LocalPlayer.CharacterAdded:Connect(function()
-    if not LocalPlayer then
-        repeat task.wait() until Player
-    end
-    if not LocalPlayer.Character then
-        repeat task.wait() until LocalPlayer.Character
-    end
-    if not LocalPlayer.Character:FindFirstChildOfClass("Humanoid") then
-        repeat task.wait() until LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-    end
-    if not LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-        repeat task.wait() until LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-    end
-    if not LocalPlayer.Character:FindFirstChild("Head") then
-        repeat task.wait() until LocalPlayer.Character:FindFirstChild("Head")
-    end
-    for i,v in pairs(game.Players:GetPlayers()) do
-        if v ~= LocalPlayer then
-            for i,DestroyPart in pairs(v.Character:GetDescendants()) do
-                if v:IsA("NoCollisionConstraint") then
-                    Debris:AddItem(v, 1/0)
-                end
-            end
-        end
-    end
-    NoCol()
-end)
+for i, v in pairs(Players:GetPlayers()) do
+	if v ~= LocalPlayer then
+		Insert(v, false)
+	end
+end;
+
+Players.PlayerAdded:Connect(Insert, false)
+
+local Reset = function()
+	if not LocalPlayer.Character then
+		LocalPlayer.CharacterAdded:Wait()
+	end;
+	LocalPlayer.Character:WaitForChild("Head")
+	LocalPlayer.Character:WaitForChild("Humanoid")
+	for i, v in pairs(Players:GetPlayers()) do
+		if v ~= LocalPlayer then
+			pcall(function()
+				for i, v in pairs(v.Character:GetDescendants()) do
+					if v.IsA(v, "NoCollisionConstraint") then
+						v:Destroy()
+					end
+				end
+			end)
+			Insert(v, true)
+		end
+	end
+end;
+
+LocalPlayer.CharacterAdded:Connect(Reset)
