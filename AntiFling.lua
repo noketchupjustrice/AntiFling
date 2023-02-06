cf7951ee83b91f0631689132aca91dd35fa7d4d1
Lucid1337 <101951687+noketchupjustrice@users.noreply.github.com>
2023-02-05 18:39:44 -0800

    Update AntiFling.lua

--- AntiFling.lua
+++ AntiFling.lua
@@ -1,4 +1,4 @@
---noketchupjustrice#3666
+--If you are going to use to share out to the public credit me you skid.
 --Everything should be fixed was just debating wether I should release this or not.
 if not game:IsLoaded() then
 	game.IsLoaded:Wait()
@@ -8,17 +8,17 @@ local Players = game:service("Players")
 local LocalPlayer = Players.LocalPlayer
 
 if not LocalPlayer.Character then
-	LocalPlayer.CharacterAdded:Wait()
+	repeat task.wait() until LocalPlayer.Character
 end;
 
 local CheckInstance = function(Part)
 	if Part.ClassName:lower():find("part") then
-        local BaseInstance = Part;
+		local BaseInstance = Part;
 		pcall(function()
 			for i, Part in pairs(LocalPlayer.Character:GetDescendants()) do
 				if Part.ClassName:lower():find("part") then
 					local NoCollisionConstraint = Instance.new("NoCollisionConstraint", BaseInstance)
-                    NoCollisionConstraint.Enabled = true
+					NoCollisionConstraint.Enabled = true
 					NoCollisionConstraint.Part0 = BaseInstance;
 					NoCollisionConstraint.Part1 = Part;
 				end
@@ -35,22 +35,24 @@ local Insert = function(Target, Bool)
 		end;
 		Target.Character:WaitForChild("Head", 5)
 		Target.Character:WaitForChild("Humanoid", 5)
-        pcall(function()
-            for i, v in pairs(Target.Character:GetDescendants()) do
-                if v:IsA("NoCollisionConstraint") then
-                    v:Destroy()
-                end
-            end;
-            for i, v in pairs(Target.Character:GetDescendants()) do
-                CheckInstance(v)
-            end;
-            Target.Character.DescendantAdded:Connect(function(v)
-                if not table.find(Inserted, v) then
-                    table.insert(Inserted, v)
-                    CheckInstance(v)
-                end
-            end)
-        end)
+		spawn(function()
+			pcall(function()
+				for i, v in pairs(Target.Character:GetDescendants()) do
+					if v:IsA("NoCollisionConstraint") then
+						v:Destroy()
+					end
+				end;
+				for i, v in pairs(Target.Character:GetDescendants()) do
+					CheckInstance(v)
+				end;
+				Target.Character.DescendantAdded:Connect(function(v)
+					if not table.find(Inserted, v) then
+						table.insert(Inserted, v)
+						CheckInstance(v)
+					end
+				end)
+			end)
+		end)
 	end;
 	pcall(Start)
 	if not Bool then
